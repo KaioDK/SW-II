9d4cf823afacd05546efa13de75a6bc0535da0c4
KaioDK <klmeronho@gmail.com>
2026-03-12 11:51:49 -0300

    add

--- Aula_03/processa.php
+++ Aula_03/processa.php
@@ -10,8 +10,28 @@
         $nome = $_POST['nome'];
         $idade = $_POST['idade'];
         $email = $_POST['email'];
+        $ano = date('Y');
+        $nasc = $ano - $idade;
 
-        echo $nome;
+        //echo $nome
+        // echo "<p>O nome é: $nome</p>";
+        // echo "<p>A idade é: $idade</p>";
+        // echo "<p>O email é: $email</p>";
     ?>
-</body>
+
+    <p>O nome é: <?php echo $nome?></p>
+    <p>A idade é: <?php echo $idade?></p>
+    <p>O email é: <?php echo $email?></p>
+    <p>Seu ano de nascimento é: <?php echo $nasc ?> </p>
+    <hr>
+
+    <?php
+        if ($idade < 18){
+            echo "<p style = 'color: blue'> Você é menor de idade! </p>";
+        }
+        else
+            echo "<p style = 'color: red'> Você é maior de idade! </p>";
+
+    ?>
+    </body>
 </html>

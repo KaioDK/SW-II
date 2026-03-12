adab9d98615d0fcc70d76628f383c59448cd781b
KaioDK <klmeronho@gmail.com>
2026-03-12 11:24:10 -0300

    start

---
+++ Aula_03/processa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processa</title>
+</head>
+<body>
+    <?php 
+        $nome = $_POST['nome'];
+        $idade = $_POST['idade'];
+        $email = $_POST['email'];
+
+        echo $nome;
+    ?>
+</body>
+</html>
